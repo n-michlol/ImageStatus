f4d108c1793616862cf37968396e589bbce1bea4
mici-ym <mrdky1@hotmail.com>
2024-08-08 19:29:34 +0300

    Switch hoc, add code in decoding the image

--- src/Hooks.php
+++ src/Hooks.php
@@ -19,20 +19,36 @@
 
 namespace MediaWiki\Extension\ImageStatus;
 
-use MediaWiki\Hook\BeforeParserFetchFileAndTitleHook;
+use MediaWiki\Hook\ImageBeforeProduceHTMLHook;
 use MediaWiki\Hook\BeforePageDisplayHook;
 use MediaWiki\Preferences\Hook\GetPreferencesHook;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Linker\DummyLinker;
+use MediaWiki\Title\Title;
+use Parser;
+use Wikimedia\Parsoid\Ext\JSON\JSON;
 
-class Hooks implements BeforeParserFetchFileAndTitleHook, BeforePageDisplayHook, GetPreferencesHook {
-
+class Hooks implements ImageBeforeProduceHTMLHook, BeforePageDisplayHook, GetPreferencesHook {
 
 	/**
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/BeforeParserFetchFileAndTitle
-	 * 
+     * @see https://www.mediawiki.org/wiki/Manual:Hooks/ImageBeforeProduceHTML
+     * @param string &$html
+     * @param array &$args
+     * @param \Image $image
 	 */
-	public function onBeforeParserFetchFileAndTitle( $parser, $nt, &$options, &$descQuery ): void {
-		$title = $nt;
+	public function onImageBeforeProduceHTML( $linker, &$title, &$file, &$frameParams, &$handlerParams, &$time, &$res, $parser, &$query, &$widthOption ) {
+		wfDebugLog('ImageStatus', "onImageBeforeProduceHTML: Processing image: ". $widthOption);
+		/*
+		$status = $this->getImageStatus($file) ?? false;
+		if ( $status == "block" ) {
+			$html = '<div style="color:red; font-weight:bold;">Image blocked</div>';
+            $res = $html;
+            return false;
+		}
+		if (!$status) {
+			$frameParams['class'] = "image-status-unknown";
+		}
+			*/
 	}
 
 	/**
